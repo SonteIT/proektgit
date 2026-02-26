c1f2b7432e72f98f35bd99cd79992a8d4f39a940
SonteIT <edsaweliu@gmail.com>
2026-02-26 12:06:34 +0700

    Разобрался со скоростью выполнения задачи на перемещение нулей с CodeWars

--- codewars/5_MoveZero.py
+++ codewars/5_MoveZero.py
@@ -1,15 +1,15 @@
-# def move_zeros(lst):
-#     res_lst = []
-#     count_zero = 0
-#     for el in lst:
-#         if el != 0:
-#             res_lst.append(el)
-#         else:
-#             count_zero += 1
-#     return res_lst + [0] * count_zero
-# print(move_zeros([1,0,2,0,3]))
+def move_zeros(lst):
+    res_lst = []
+    count_zero = 0
+    for el in lst:
+        if el != 0:
+            res_lst.append(el)
+        else:
+            count_zero += 1
+    return res_lst + [0] * count_zero
+print(move_zeros([1,0,2,0,3]))  #вариант работает за O(n) времени
 
 
 def move_zeros(lst):
     return sorted(lst, key=lambda x: x == 0)
-print(move_zeros([1,0,2,0,3]))
+print(move_zeros([1,0,2,0,3]))  #вариант работает за O(n log n) времени
